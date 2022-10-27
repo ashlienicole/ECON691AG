c58b3212ba76ff8e3e796886a8a6e70ae4826715
Ashlie Garner <z1940977@students.niu.edu>
2022-10-26 19:03:08 -0500

    Initial commit

---
+++ Build/Code/Data Gathering.R
@@ -0,0 +1,10 @@
+#Econ 691 Homework#1
+#Ashlie Garner
+#Oct 26, 2022
+
+rm(list=ls())
+
+library(tidyverse)
+
+covid<-read.csv("https://raw.githubusercontent.com/nytimes/
+                covid-19-data/master/us-contries.csv")
